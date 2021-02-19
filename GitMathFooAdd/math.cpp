5947779d2d4db1bc33f44a697da05a88f962c928
Andriy <andriylytvyn46@gmail.com>
2021-02-20 00:43:00 +0300

    Result math foo add

---
+++ GitMathFooAdd/math.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+void add(int digit1, int digit2)
+{
+	int result_add = digit1 + digit2;
+	cout << result_add << endl;
+}
+
+int main()
+{
+	add(5, 5);
+	return 0;
+}
